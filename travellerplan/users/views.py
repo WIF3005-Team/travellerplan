b0b2fb85492866beea895b7572918c0f00fb0d25
sarahsyazwina <48936987+sarahsyazwina@users.noreply.github.com>
2021-01-28 02:43:14 +0800

    registration maintenance

--- travellerplan/users/views.py
+++ travellerplan/users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
 # Create your views here.
+# message.debug
+# message.info
+# message.success
+# message.warning
+# message.error
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('register')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form' : form, 'nav':'register'})
