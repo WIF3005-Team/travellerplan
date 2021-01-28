b699f7c7b8c1f94c3a88fac15d8cda62d305aef8
syzsrh <62366893+farahin98@users.noreply.github.com>
2021-01-28 11:04:07 +0800

    update

--- travellerplan/travellerplan/settings.py
+++ travellerplan/travellerplan/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 
 # Application definition
 
@@ -39,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
     'users.apps.UsersConfig',
+    'weather.apps.WeatherConfig',
     'crispy_forms',
 ]
 
