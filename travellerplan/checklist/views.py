bd77cf70bd75390cbee0b62236f3e570b44bbb65
Nur Farahin Al Latiff <62366893+farahin98@users.noreply.github.com>
2021-01-28 15:12:33 +0800

    maintenance checklist

--- travellerplan/checklist/views.py
+++ travellerplan/checklist/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
 
+from .models import Todo
 # Create your views here.
 
 @login_required(login_url="login")
 def checklist(request):
-    result = {}
-    return render(request, 'checklist/checklist.html', {'nav':'checklist','result':result})
+
+	result = {'1':1}
+	return render(request, 'checklist/checklist.html', {'nav':'checklist','result':result})
+
